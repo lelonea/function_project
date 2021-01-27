9f8c9970312c4b62537ec69a43aefd458c9f24d4
Nastya <leloneya@gmail.com>
2021-01-27 20:12:48 +0200

    done task func

--- 2task.py
+++ 2task.py
@@ -1,9 +1,29 @@
-def fact(n):
+# def fact(n):
+#     ind = 0
+#     res = 1
+#     while ind < n:
+#         ind +=1
+#         res *= ind
+#     print(res)
+#     return res
+# fact(5)
+#
+# def form(string, char='!'):
+#     res_string = f'{char} {string} {char}'
+#
+#     return res_string
+#
+#
+# print(form('nast'))
+
+def pl(*args):
+    b = args
     ind = 0
-    res = 1
-    while ind < n:
-        ind +=1
-        res *= ind
+    res = 0
+    for i in range(1, b[-1], 1):
+       res += b[ind] * b[-1]
+
     print(res)
-    return res
-fact(5)
+
+pl(1, 2, 5)
+
