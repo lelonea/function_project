9f470ae45b26603f396f84581e79bdc3f398cf68
Nastya <leloneya@gmail.com>
2021-01-27 19:08:10 +0200

    created new files with tasks

---
+++ task.py
@@ -0,0 +1,6 @@
+def hello(a):
+    b = 'Hello' + a
+    print('Hello, ', f'{a}')
+    return b
+hello('Nastya')
+
--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print('Hell')
+
+def func(a, b):
+    summ = a + b
+    print(f'{a} + {b} = {summ}')
+    return summ
+a = func(4, 6)
+print(a)
+
