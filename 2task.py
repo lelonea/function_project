e74b6ba96b0c2921c8066a18f0242eea44ee02e5
Nastya <leloneya@gmail.com>
2021-01-31 14:46:42 +0200

    end lesson 6

--- 2task.py
+++ 2task.py
@@ -16,16 +16,51 @@
 #
 # print(form('nast'))
 
-def pl(*args):
-    b = args
-    ind = 0
-    res = 0
-    for i in b:
-        res += b[ind] * b[-1]
-        ind += 1
-    print(res)
-    print(b)
-    return b
+# def pl(*args):
+#     b = args
+#     ind = 0
+#     res = 0
+#     for i in b:
+#         res += b[ind] * b[-1]
+#         ind += 1
+#     print(res)
+#     print(b)
+#     return b
+#
+# pl(1, 2, 5)
 
-pl(1, 2, 5)
+# def func(a, b):
+#     summ = a + b
+#     digg = a - b
+#     return summ, digg
+# summr, diggr = func(3, 4)
+# print(summr)
 
+# def func(*args):
+#     res = 0
+#     ind = 0
+#     b = args
+#     max = 0
+#     for i in b:
+#         res += b[ind]
+#         ind += 1
+#         if i > b[-1] and i > max:
+#             max = i
+#     print(max)
+#     print(res)
+#
+# func(1, 4, 3, 5, 8)
+# not working!!!
+
+
+# def func(**kwargs):
+#     for k, w in kwargs.items():
+#         print(k, w)
+#         print(kwargs)
+#
+# func(a=5)
+
+def dict(*args, **kwargs):
+    print(args)
+    print(kwargs)
+dict( a = 5, c = 5)
