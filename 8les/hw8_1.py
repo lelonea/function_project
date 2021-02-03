18cb2df00b2f7f3167101b61a9cd55e6e15b76cf
Nastya <leloneya@gmail.com>
2021-02-03 07:55:58 +0200

    hw8

---
+++ 8les/hw8_1.py
@@ -0,0 +1,9 @@
+a = ['the', 'dog', 'was', 'called', 'a', 'hero']
+
+f = [f'{a.index(n)} - {n}' for n in a]
+print(f)
+
+# c ={89: 'jkm', 'lkmn': 900, 'nono': 'iikl'}
+# b = map(lambda x, y: f'{x}{x}', c)
+# # x = input(**kwargs)
+# print(dict(b))
